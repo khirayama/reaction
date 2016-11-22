e26422c7f4010ae0bc77549785ade885855b020a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-11-23 06:58:27 +0900

    Create stylesheet task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+
 const browserify = require('browserify');
 const watchify = require('watchify');
 const source = require('vinyl-source-stream');
-const plumber = require('gulp-plumber');
+
+const postcss = require('gulp-postcss');
+const csssimplevars = require('postcss-simple-vars');
+const cssnested = require('postcss-nested');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 const SRC_ROOT = 'client';
 const DIST_ROOT = 'public';
@@ -12,19 +19,49 @@ const options = {
     browserify: {
       entries: [`${SRC_ROOT}/index.js`],
       transform: ['babelify'],
-      extensions: ['.jsx', '.js'],
     },
     watchify: {
       entries: [`${SRC_ROOT}/index.js`],
       transform: ['babelify'],
       debug: true,
-      extensions: ['.jsx', '.js'],
       cache: {},
       packageCache: {},
       plugin: [watchify],
     },
   },
+  styles: {
+    autoprefixer: {
+      browsers: ['last 2 versions'],
+    }
+  },
 };
+
+function buildStyles(isWatch) {
+  function build() {
+    console.log('build: styles');
+
+    const processors = [
+      csssimplevars,
+      cssnested,
+      autoprefixer(options.styles.autoprefixer),
+      cssnano,
+    ];
+    return gulp.src(`${SRC_ROOT}/styles/index.css`)
+      .pipe(plumber())
+      .pipe(postcss(processors))
+      .pipe(gulp.dest(DIST_ROOT));
+  }
+
+  if (isWatch) {
+    return () => {
+      build();
+      gulp.watch(`${SRC_ROOT}/styles/**/*.css`, build);
+    };
+  }
+  return () => {
+    build();
+  };
+}
 
 function buildScripts(isWatch) {
   const options_ = (isWatch) ? options.scripts.watchify : options.scripts.browserify;
@@ -46,6 +83,9 @@ function buildScripts(isWatch) {
 }
 
 // tasks
+gulp.task('build:styles', buildStyles(false));
+gulp.task('watch:styles', buildStyles(true));
 gulp.task('build:scripts', buildScripts(false));
 gulp.task('watch:scripts', buildScripts(true));
-gulp.task('watch', ['watch:scripts']);
+gulp.task('build', ['build:styles', 'build:scripts']);
+gulp.task('watch', ['watch:styles', 'watch:scripts']);
