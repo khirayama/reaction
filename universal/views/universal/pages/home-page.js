ef2cb1b6d717b52f5f05b84a271625049f3ee01a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-12-03 13:48:04 +0900

    Enable language setting

--- universal/views/universal/pages/home-page.js
+++ universal/views/universal/pages/home-page.js
@@ -8,6 +8,10 @@ export default function HomePage(props) {
       <h1>Reaction</h1>
       <p>{i18n.t('description')}</p>
       <a href="/auth/twitter">Login with Twitter</a>
+      <ul>
+        <li><a href="?lang=en">English</a></li>
+        <li><a href="?lang=ja">日本語</a></li>
+      </ul>
     </section>
   );
 }
