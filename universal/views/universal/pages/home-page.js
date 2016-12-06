58f7d7df62390340fd420288d932ea6a485daa76
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-12-06 22:22:35 +0900

    Update home-page styles

--- universal/views/universal/pages/home-page.js
+++ universal/views/universal/pages/home-page.js
@@ -4,14 +4,18 @@ import i18n from 'universal/locales';
 
 export default function HomePage() {
   return (
-    <section>
-      <h1>Reaction</h1>
-      <p>{i18n.t('description')}</p>
-      <a href="/auth/twitter">Login with Twitter</a>
-      <ul>
-        <li><a href="?lang=en">English</a></li>
-        <li><a href="?lang=ja">日本語</a></li>
-      </ul>
+    <section className="page home-page">
+      <header>
+        <h1>Reaction</h1>
+        <p>{i18n.t('home.description')}</p>
+        <a href="/auth/twitter">Login with Twitter</a>
+      </header>
+      <div className="lang-list-container">
+        <ul className="lang-list">
+          <li><a href="?lang=en"><small>English</small></a></li>
+          <li><a href="?lang=ja"><small>日本語</small></a></li>
+        </ul>
+      </div>
     </section>
   );
 }
